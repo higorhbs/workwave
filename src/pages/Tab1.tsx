7a8424344f573101ae99e8f15f784efe8e7a26bd
higorhbs <higorhbs99@outlook.com>
2024-04-15 21:30:56 -0300

    Criação de pagina inicial

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,25 +1,24 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonImg } from '@ionic/react';
 import './Tab1.css';
 
-const Tab1: React.FC = () => {
-  return (
-    <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Tab 1</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Tab 1</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer name="Tab 1 page" />
-      </IonContent>
-    </IonPage>
-  );
-};
+const YourComponent: React.FC = () => (
+  <IonPage style={{ backgroundColor: 'white' }}>
+    <div className="ion-padding">
+      <div className="logo">
+        <IonImg src="./resources/icone.png"></IonImg>
+      </div>
+    </div>
+    <div className="nomeapp">
+      WORKWAVE
+      <div className='subtitulo'>
+        WORK APP
+      </div>
+    </div>
+    <div className='eslogan'>
+      Ondas de facilidade e conveniência ao alcance das suas mãos!
+    </div>
 
-export default Tab1;
+  </IonPage>
+);
+
+export default YourComponent;
--- src/pages/Tab2.tsx
+++ src/pages/Tab2.tsx
@@ -7,13 +7,13 @@ const Tab2: React.FC = () => {
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Tab 2</IonTitle>
+          <IonTitle>Modelos DWTT As5</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
         <IonHeader collapse="condense">
           <IonToolbar>
-            <IonTitle size="large">Tab 2</IonTitle>
+            <IonTitle size="large">Tab 23</IonTitle>
           </IonToolbar>
         </IonHeader>
         <ExploreContainer name="Tab 2 page" />
