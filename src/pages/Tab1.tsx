ab67f6b5e17b185a6bcabfc835f96bc700b55f19
higorhbs <higorhbs99@outlook.com>
2024-04-15 22:22:39 -0300

    tela de login

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,5 +1,6 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonImg } from '@ionic/react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonImg, IonInput, IonItem, IonLabel, IonButton, IonList } from '@ionic/react';
 import './Tab1.css';
+import React, { useState } from 'react';
 
 const YourComponent: React.FC = () => (
   <IonPage style={{ backgroundColor: 'white' }}>
@@ -20,5 +21,38 @@ const YourComponent: React.FC = () => (
 
   </IonPage>
 );
+
+
+
+const LoginPage: React.FC = () => {
+  const [username, setUsername] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+
+  const handleLogin = () => {
+    // Lógica de login aqui
+    console.log('Usuário:', username, 'Senha:', password);
+    // Você pode adicionar aqui a lógica de autenticação, navegação para a próxima página, etc.
+  };
+
+  return (
+    <IonContent>
+      <IonList>
+        <IonItem>
+          <IonLabel position="floating">Nome de usuário</IonLabel>
+          <IonInput type="text" value={username} onIonChange={e => setUsername(e.detail.value!)}></IonInput>
+        </IonItem>
+        
+        <IonItem>
+          <IonLabel position="floating">Senha</IonLabel>
+          <IonInput type="password" value={password} onIonChange={e => setPassword(e.detail.value!)}></IonInput>
+        </IonItem>
+      </IonList>
+      
+      <IonButton expand="block" onClick={handleLogin}>Login</IonButton>
+    </IonContent>
+  );
+};
+
+
 
 export default YourComponent;
--- src/pages/Tab2.tsx
+++ src/pages/Tab2.tsx
@@ -1,6 +1,7 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonHeader, IonPage, IonTitle, IonToolbar, IonContent, IonInput, IonItem, IonLabel, IonButton, IonList } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Tab2.css';
+import React, { useState } from 'react';
 
 const Tab2: React.FC = () => {
   return (
@@ -16,10 +17,39 @@ const Tab2: React.FC = () => {
             <IonTitle size="large">Tab 23</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer name="Tab 2 page" />
+        <LoginPage /> {/* Renderize o componente LoginPage aqui */}
       </IonContent>
     </IonPage>
   );
 };
 
-export default Tab2;
+const LoginPage: React.FC = () => {
+  const [username, setUsername] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+
+  const handleLogin = () => {
+    // Lógica de login aqui
+    console.log('Usuário:', username, 'Senha:', password);
+    // Você pode adicionar aqui a lógica de autenticação, navegação para a próxima página, etc.
+  };
+
+  return (
+    <IonContent>
+      <IonList>
+        <IonItem>
+          <IonLabel position="floating">Nome de usuário</IonLabel>
+          <IonInput type="text" value={username} onIonChange={e => setUsername(e.detail.value!)}></IonInput>
+        </IonItem>
+        
+        <IonItem>
+          <IonLabel position="floating">Senha</IonLabel>
+          <IonInput type="password" value={password} onIonChange={e => setPassword(e.detail.value!)}></IonInput>
+        </IonItem>
+      </IonList>
+      
+      <IonButton expand="block" onClick={handleLogin}>Login</IonButton>
+    </IonContent>
+  );
+};
+
+export default LoginPage;
